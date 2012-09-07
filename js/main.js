f4a329b6a98715e34a856336482285e77acbc07b
Sander van konijnenenburg <sander92@xs4all.nl>
2012-09-07 15:16:30 +0200

    Added capture foto

--- js/main.js
+++ js/main.js
@@ -220,12 +220,60 @@ function init() {
     // document.addEventListener("touchmove", preventBehavior, false);
  //   document.addEventListener("deviceready", deviceInfo, true);
 		alert(window.screen.height);
-		if (window.screen.height == 990) 
+		if (window.screen.height == 1080) 
 		{
-    		document.getElementById('mainWebsite').style.height='870px';
+    		document.getElementById('mainWebsite').style.height='977px';
    	}
    		if (window.screen.height == 1280) 
 		{
     		document.getElementById('mainWebsite').style.height='600px';
    		}
    }
+function showPhotoView()
+{
+	document.getElementById('menu').style.display='none';
+	document.getElementById('photoview').style.display='block';
+}
+function capturePhoto(){
+    navigator.camera.getPicture(uploadPhoto,null,{sourceType:1,quality:60});
+}
+function uploadPhoto(data){
+// this is where you would send the image file to server
+ 
+//output image to screen
+    cameraPic.src = "data:image/jpeg;base64," + data;
+}
+function uploadPhoto(data){
+// this is where you would send the image file to server
+ 
+    cameraPic.src = "data:image/jpeg;base64," + data;
+    // Successful upload to the server
+    navigator.notification.alert(
+        'Your Photo has been uploaded',  // message
+        okay,                           // callback
+        'Photo Uploaded',              // title
+        'OK'                          // buttonName
+    );
+ 
+    // upload has failed Fail
+ 
+    
+ 
+    if (failedToUpload){
+ 
+    navigator.notification.alert(
+        'Your Photo has failed to upload',
+        failedDismissed,
+        'Photo Not Uploaded',
+        'OK'
+        );
+ 
+    } 
+
+
+ 
+}
+ 
+function okay(){
+    // Do something
+}
